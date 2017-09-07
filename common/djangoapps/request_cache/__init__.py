95d79fea066971ac55dc1e69fdd70d4c8d7fca16
Julien Tayon <julien.tayon@fun-mooc.fr>
2017-09-07 13:49:50 +0200

    Mem leak

--- common/djangoapps/request_cache/__init__.py
+++ common/djangoapps/request_cache/__init__.py
@@ -8,6 +8,7 @@ is installed in order to clear the cache after each request.
 import logging
 from urlparse import urlparse
 
+from celery.signals import task_postrun
 from django.conf import settings
 from django.test.client import RequestFactory
 
@@ -15,6 +16,14 @@ from request_cache import middleware
 
 
 log = logging.getLogger(__name__)
+
+@task_postrun.connect
+def clear_request_cache(**kwargs):  # pylint: disable=unused-argument
+    """
+    Once a celery task completes, clear the request cache to
+    prevent memory leaks.
+    """
+    middleware.RequestCache.clear_request_cache()
 
 
 def get_cache(name):
